6096b3a4281dd936574924e9352e0c119b3b33cd
Hector Romero <hectorromerodev@gmail.com>
2022-06-30 15:02:24 -0700

    💄 no-data-found styles and structure updated

--- src/app/modules/shared/components/no-data-found/no-data-found.component.ts
+++ src/app/modules/shared/components/no-data-found/no-data-found.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-no-data-found',
   templateUrl: './no-data-found.component.html',
   styleUrls: ['./no-data-found.component.scss']
 })
-export class NoDataFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NoDataFoundComponent {
+  @Input() title: string = 'Any data was found';
+  @Input() message: string = 'Sorry, we can\'t find what you are looking for.';
+  img = {
+    height: 300,
+    width: 500,
+    src: 'assets/imgs/empty.svg'
+  };
 }
