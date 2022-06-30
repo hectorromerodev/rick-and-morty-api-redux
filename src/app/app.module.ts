68175ddfb33c170a6aa018b5a3105f69414d0596
Hector Romero <hectorromerodev@gmail.com>
2022-06-30 12:30:24 -0700

    🎨 app module ngrx modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
-import { ROOT_REDUCERS } from '@state/app.state';
+
+import { environment } from '../environments/environment';
+import { ROOT_REDUCERS, ROOT_EFFECTS } from '@state/app.state';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,10 @@ import { ROOT_REDUCERS } from '@state/app.state';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     StoreModule.forRoot(ROOT_REDUCERS),
+    EffectsModule.forRoot(ROOT_EFFECTS),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
