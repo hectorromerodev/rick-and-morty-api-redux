27583a997b68fd55428f18f7f242c9fc19e8a811
Hector Romero <hectorromerodev@gmail.com>
2022-06-30 14:37:19 -0700

    💄spinner-loader style and structure updated

--- src/app/modules/shared/components/spinner-loader/spinner-loader.component.ts
+++ src/app/modules/shared/components/spinner-loader/spinner-loader.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SpinnerLoaderService } from '@services/spinner-loader.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-spinner-loader',
   templateUrl: './spinner-loader.component.html',
   styleUrls: ['./spinner-loader.component.scss']
 })
-export class SpinnerLoaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SpinnerLoaderComponent {
+  isLoading$: Subject<boolean> = this.spinnerLoaderSvc.isLoading$;
+  constructor(private spinnerLoaderSvc: SpinnerLoaderService) { }
 }
